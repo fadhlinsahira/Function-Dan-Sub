13e674fc24f936c7bc0d71e90583dbcda890ec6a
Nurul Fadhlin Sahira Binti Mohd Asri <18DDT19F1022@student.ptss.edu.my>
2021-04-20 13:32:35 +0800

    buat function dan sub

--- Function Dan Sub/Form1.vb
+++ Function Dan Sub/Form1.vb
@@ -1,3 +1,24 @@
 ﻿Public Class Form1
+    Private Sub btn1_Click(sender As Object, e As EventArgs) Handles btn1.Click
+        lbl1.Text = "Data tersebut"
+    End Sub
+
+    Private Sub btn2_Click(sender As Object, e As EventArgs) Handles btn2.Click
+        Dim strText As String
+        strText = txt1.Text
+        lbl2.Text = strText
+    End Sub
+
+    Private Function btn3_Click(sender As Object, e As EventArgs) Handles btn3.Click
+        lbl3.Text = "Data tersebut"
+        Return lbl3
+    End Function
+
+    Private Function btn4_Click(sender As Object, e As EventArgs) Handles btn4.Click
+        Dim strText2 As String
+        strText2 = txt2.Text
+        lbl4.Text = strText2
+        Return lbl4
+    End Function
 
 End Class
