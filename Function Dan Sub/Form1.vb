b176132bca2dddf8daa05dc814a951e538407b5a
Nurul Fadhlin Sahira Binti Mohd Asri <18DDT19F1022@student.ptss.edu.my>
2021-04-20 21:21:07 +0800

    latest function dan sub

--- Function Dan Sub/Form1.vb
+++ Function Dan Sub/Form1.vb
@@ -1,24 +1,35 @@
 ﻿Public Class Form1
-    Private Sub btn1_Click(sender As Object, e As EventArgs) Handles btn1.Click
+    Private Sub S1()
         lbl1.Text = "Data tersebut"
     End Sub
+    Private Sub btn1_Click(sender As Object, e As EventArgs) Handles btn1.Click
+        S1()
+    End Sub
 
-    Private Sub btn2_Click(sender As Object, e As EventArgs) Handles btn2.Click
+    Private Sub S2()
         Dim strText As String
         strText = txt1.Text
         lbl2.Text = strText
     End Sub
+    Private Sub btn2_Click(sender As Object, e As EventArgs) Handles btn2.Click
+        S2()
+    End Sub
 
+    Private Function F1()
+        Return "DATA TERSEBUT"
+    End Function
     Private Function btn3_Click(sender As Object, e As EventArgs) Handles btn3.Click
-        lbl3.Text = "Data tersebut"
-        Return lbl3
+        lbl3.Text = F1()
+    End Function
+
+    Private Function F2()
+        Dim strText2 As String
+        strText2 = txt2.Text
+        Return strText2
     End Function
 
     Private Function btn4_Click(sender As Object, e As EventArgs) Handles btn4.Click
-        Dim strText2 As String
-        strText2 = txt2.Text
-        lbl4.Text = strText2
-        Return lbl4
+        lbl4.Text = F2()
     End Function
 
 End Class
